ffca457df57d452de2f97cb2c4ecdbbce49b9474
Daniel Silva <danielsilva@bossabox.com>
2022-03-27 11:16:18 -0300

    feat: resolved typescript issues

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,11 @@
-import { useEffect } from "react";
-import type {
-  GetServerSideProps,
-  GetStaticPaths,
-  GetStaticProps,
-  NextPage,
-} from "next";
+import type { NextPage } from "next";
 
-const Home: NextPage = ({ data }) => {
+const Home: NextPage = () => {
   return (
     <div>
       <h1>Hello!</h1>
-      <p>{JSON.stringify(data)}</p>
     </div>
   );
-};
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  const response = await fetch("http://localhost:3000/api/get");
-  const data = await response.json();
-
-  return {
-    props: {
-      data,
-    },
-  };
 };
 
 export default Home;
