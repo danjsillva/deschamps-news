7af9b81a71a698de1cb65b758b50eed67ff227a1
Daniel Silva <danielsilva@bossabox.com>
2022-03-30 18:03:44 -0300

    fix: test save database with promise

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -12,14 +12,16 @@ export default async function handler(
 
     await client.connect();
 
-    const result = await client.json.set("post", ".", req.body);
+    /* const result = await client.json.set("post", ".", req.body); */
 
-    const posts = req.body
+    const postsHtml = req.body
       .replace(/<span.*?>&nbsp;<\/span>/gi, " ")
-      .match(/<p .*?<\/p>/gi)
-      .map(async (item: string) => {
-        const text = item.replace(/(<([^>]+)>)/gi, "");
-        const html = item
+      .match(/<p .*?<\/p>/gi);
+
+    const posts = await Promise.all(
+      postsHtml.map(async (postHtml: string) => {
+        const text = postHtml.replace(/(<([^>]+)>)/gi, "");
+        const html = postHtml
           .replace(/ class=\".*?\"/gm, "")
           .replace(/ style=\".*?\"/gm, "");
         const post = {
@@ -37,15 +39,37 @@ export default async function handler(
           ".",
           post
         );
+      })
+    );
+    /* .map(async (item: string) => { */
+    /*   const text = item.replace(/(<([^>]+)>)/gi, ""); */
+    /*   const html = item */
+    /*     .replace(/ class=\".*?\"/gm, "") */
+    /*     .replace(/ style=\".*?\"/gm, ""); */
+    /*   const post = { */
+    /*     html, */
+    /*     text, */
+    /*     categories: [], */
+    /*     entities: [], */
+    /*     keywords: [], */
+    /*     likes: 0, */
+    /*     date: new Date(), */
+    /*   }; */
 
-        return result;
-      });
+    /*   const result = await client.json.set( */
+    /*     new Date().getTime().toString(), */
+    /*     ".", */
+    /*     post */
+    /*   ); */
+
+    /*   return result; */
+    /* }); */
 
     await client.quit();
 
     res.status(200).json({
       status: "success",
-      data: result,
+      data: posts,
     });
   } catch (error) {
     console.log(error);
