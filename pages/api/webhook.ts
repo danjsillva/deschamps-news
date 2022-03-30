f976e2134fdb9012d7a759b2bb629dbe0e3686dd
Daniel Silva <danielsilva@bossabox.com>
2022-03-30 15:15:25 -0300

    feat: added save posts on database

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -1,18 +1,38 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { createClient } from "redis";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   try {
-    const { body, method } = req;
-
-    console.log(body, method);
-
-    res.status(200).json({
-      message: "success",
-      body,
+    const client = createClient({
+      url: `redis://:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
     });
+
+    await client.connect();
+
+    const posts = req.body
+      .replace(/<span.*?>&nbsp;<\/span>/gi, " ")
+      .match(/<p .*?<\/p>/g)
+      .map(async (item: string) => {
+        const text = item.replace(/(<([^>]+)>)/gi, "");
+        const html = item
+          .replace(/ class=\".*?\"/gm, "")
+          .replace(/ style=\".*?\"/gm, "");
+
+        await client.json.set(new Date().getTime().toString(), ".", {
+          html,
+          text,
+          categories: [],
+          entities: [],
+          keywords: [],
+          likes: 0,
+          date: new Date(),
+        });
+      });
+
+    res.status(201).json();
   } catch (error) {
     console.log(error);
 
