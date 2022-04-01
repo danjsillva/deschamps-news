c9dcd438e681f136fbe8802d62fb43e2e9140fb1
Daniel Silva <danielsilva@bossabox.com>
2022-03-31 23:26:15 -0300

    chore: removed static props (deploy api only)

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,49 +18,23 @@ interface Post {
 }
 
 const Home: NextPage<Props> = ({ postsDate, posts }) => {
-  return (
-    <main>
-      <section>
-        <div className="date-group">
-          <span className="date-day">
-            {dayjs(postsDate).utc().format("DD")}
-          </span>
-          <div className="date-month-year-group">
-            <span className="date-month">
-              {dayjs(postsDate).utc().format("MMMM")}
-            </span>
-            <span className="date-year">
-              {dayjs(postsDate).utc().format("YYYY")}
-            </span>
-          </div>
-        </div>
-
-        {posts.map((post) => (
-          <article
-            key={post.id}
-            dangerouslySetInnerHTML={{ __html: post.html }}
-            className="post-item"
-          />
-        ))}
-      </section>
-    </main>
-  );
+  return <main></main>;
 };
 
-export const getStaticProps: GetStaticProps = async () => {
-  const date = dayjs().format("YYYY-MM-DD");
+/* export const getStaticProps: GetStaticProps = async () => { */
+/*   const date = dayjs().format("YYYY-MM-DD"); */
 
-  const response = await fetch(`${process.env.API_BASE_URL}/posts/${date}`);
-  const posts = await response.json();
-  const postsDate = posts[0].date;
+/*   const response = await fetch(`${process.env.API_BASE_URL}/posts/${date}`); */
+/*   const posts = await response.json(); */
+/*   const postsDate = posts[0].date; */
 
-  return {
-    props: {
-      postsDate,
-      posts,
-    },
-    revalidate: 60,
-  };
-};
+/*   return { */
+/*     props: { */
+/*       postsDate, */
+/*       posts, */
+/*     }, */
+/*     revalidate: 60, */
+/*   }; */
+/* }; */
 
 export default Home;
