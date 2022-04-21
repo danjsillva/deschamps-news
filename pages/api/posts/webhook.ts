06adcabe52642c07e13e719802ced19eb18eb41a
Daniel Silva <danielsilva@bossabox.com>
2022-04-21 11:50:58 -0300

    fix: added a valid date check

--- pages/api/posts/webhook.ts
+++ pages/api/posts/webhook.ts
@@ -16,24 +16,26 @@ export default async function handler(
     const date = dayjs(HTMLHelper.getDate(req.body));
     const paragraphs = HTMLHelper.getParagraphs(req.body);
 
-    if (date && paragraphs.length) {
-      for (const [index, paragraph] of paragraphs.entries()) {
-        const text = HTMLHelper.getText(paragraph);
-        const entities = await AWSHelper.getEntities(text);
+    if (!date.isValid() || !paragraphs.length) {
+      throw new Error("Invalid date or paragraphs");
+    }
 
-        const post = {
-          id: index + 1,
-          html: paragraph,
-          text: text,
-          categories: [],
-          entities: Array.from(new Set(entities)),
-          likes: 0,
-          date: date.format(),
-        };
+    for (const [index, paragraph] of paragraphs.entries()) {
+      const text = HTMLHelper.getText(paragraph);
+      const entities = await AWSHelper.getEntities(text);
 
-        await RedisHelper.save(post);
-        await TwitterHelper.tweet(post);
-      }
+      const post = {
+        id: index + 1,
+        html: paragraph,
+        text: text,
+        categories: [],
+        entities: Array.from(new Set(entities)),
+        likes: 0,
+        date: date.format(),
+      };
+
+      await RedisHelper.save(post);
+      await TwitterHelper.tweet(post);
     }
 
     await RedisHelper.close();
