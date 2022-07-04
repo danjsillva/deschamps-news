aa541ce231e43b16f1edb4ce9d375f04d31d497e
Daniel Silva <danielsilva@bossabox.com>
2022-07-04 17:27:58 -0300

    feat: added log for new webhook

--- pages/api/posts/webhook.ts
+++ pages/api/posts/webhook.ts
@@ -13,38 +13,38 @@ export default async function handler(
   try {
     console.log(req.body);
 
-    await RedisHelper.connect();
+    /* await RedisHelper.connect(); */
 
-    const date = dayjs(HTMLHelper.getDate(req.body));
-    const paragraphs = HTMLHelper.getParagraphs(req.body);
+    /* const date = dayjs(HTMLHelper.getDate(req.body)); */
+    /* const paragraphs = HTMLHelper.getParagraphs(req.body); */
 
-    if (!date.isValid() || !paragraphs.length) {
-      throw new Error("Invalid date or paragraphs");
-    }
+    /* if (!date.isValid() || !paragraphs.length) { */
+    /*   throw new Error("Invalid date or paragraphs"); */
+    /* } */
 
-    for (const [index, paragraph] of paragraphs.entries()) {
-      const text = HTMLHelper.getText(paragraph);
-      const entities = await AWSHelper.getEntities(text);
+    /* for (const [index, paragraph] of paragraphs.entries()) { */
+    /*   const text = HTMLHelper.getText(paragraph); */
+    /*   const entities = await AWSHelper.getEntities(text); */
 
-      const post = {
-        id: index + 1,
-        html: paragraph,
-        text: text,
-        categories: [],
-        entities: Array.from(new Set(entities)),
-        likes: 0,
-        date: date.format(),
-      };
+    /*   const post = { */
+    /*     id: index + 1, */
+    /*     html: paragraph, */
+    /*     text: text, */
+    /*     categories: [], */
+    /*     entities: Array.from(new Set(entities)), */
+    /*     likes: 0, */
+    /*     date: date.format(), */
+    /*   }; */
 
-      await RedisHelper.save(post);
-      await TwitterHelper.tweet(post);
-    }
+    /*   await RedisHelper.save(post); */
+    /*   await TwitterHelper.tweet(post); */
+    /* } */
 
-    await RedisHelper.close();
+    /* await RedisHelper.close(); */
 
-    return res.status(200).json({
-      status: "success",
-    });
+    /* return res.status(200).json({ */
+    /*   status: "success", */
+    /* }); */
   } catch (error) {
     console.error(error);
 
