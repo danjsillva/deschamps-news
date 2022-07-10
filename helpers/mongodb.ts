edbbdf25cc7d47aa2b1fc8cc42c0a07fa3812b62
Daniel Silva <danielsilva@bossabox.com>
2022-07-10 09:52:27 -0300

    fix: fixed mongodb connect function

--- helpers/mongodb.ts
+++ helpers/mongodb.ts
@@ -2,15 +2,10 @@ import { MongoClient } from "mongodb";
 
 const uri = process.env.MONGODB_URI;
 const options = {};
+const client = new MongoClient(String(uri), options);
 
 const connect = async () => {
   try {
-    if (!uri) {
-      throw new Error("MONGODB_URI is not set");
-    }
-
-    const client = new MongoClient(uri, options);
-
     await client.connect();
 
     return client.db("deschamps-news");
